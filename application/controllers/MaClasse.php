fb17af95386113d9b37406943c356e330adf0523
ginius4ginius <derieu.vincent@outlook.com>
2018-01-12 12:04:00 +0100

    commit

---
+++ application/controllers/MaClasse.php
@@ -0,0 +1,15 @@
+<?php
+Class MaClasse extends CI_Controller{
+
+  //constructeur
+  function __construc(){
+
+    parent::__construc();// appel du constructeur de CI_Controller
+    
+
+  }
+  //
+
+}
+
+?>
---
+++ application/views/Layout.php
@@ -0,0 +1,4 @@
+<h1>Conteneur principal</h1>
+<?php
+    $this->load->view("content");
+ ?>
